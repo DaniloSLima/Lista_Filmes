82407d1946fec292dc18f2ffdb0b9c6e855adf5c
DaniloSLima <danilo.lima1989@gmail.com>
2018-11-04 23:44:21 -0300

    01
    Lista de filmes

---
+++ app/src/test/java/br/usjt/sin/progmulti/listafilme/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package br.usjt.sin.progmulti.listafilme;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *@Author: Danilo de Souza Lima RA:816114969
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
